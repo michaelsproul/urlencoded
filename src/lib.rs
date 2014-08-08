62695e416f141c9030bb6a07dde9d83eb069c269
Michael Sproul <micsproul@gmail.com>
2014-08-09 00:06:15 +1000

    (feat) Switch to a mixin approach.

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,15 @@
 //! Parses "url encoded" data from client requests.
 //! Capable of parsing both URL query strings and POST request bodies.
 
-// Use rust-url over soon to be deprecated liburl
 extern crate url;
 extern crate iron;
 extern crate serialize;
 
-use iron::{Request, Response, Middleware, Status, Continue};
-
+use iron::Request;
 use url::form_urlencoded;
 use std::collections::HashMap;
 
-/// Stores data extracted from the query string and body of a request in a pair of hashmaps.
+/// Mixin that attaches parsing methods for urlencoded data to a `Request`.
 ///
 /// Each map is `Some` only if both of the following conditions are met:
 ///     1. Parsing of the field is enabled in the `UrlEncodedParser`.
@@ -25,64 +23,21 @@ use std::collections::HashMap;
 /// The values are stored in a vector so that keys which appear multiple times can map to
 /// multple values.
 /// e.g. "?a=b&a=c" is stored as `a => vec![b, c]`
-#[deriving(Clone)]
-pub struct UrlEncodedData {
-    // HashMap created from URL query string.
-    pub query_string: Option<HashMap<String, Vec<String>>>,
-
-    // HashMap created from request body.
-    pub body: Option<HashMap<String, Vec<String>>>
+pub trait UrlEncodedMixin {
+    fn parse_urlencoded_query(&self) -> Option<HashMap<String, Vec<String>>>;
+    fn parse_urlencoded_body(&self) -> Option<HashMap<String, Vec<String>>>;
 }
 
-/// Middleware which inserts a `UrlEncodedData` object into the alloy for later use.
-///
-/// Can be configured to parse the request's URL, body or both. Always inserts something.
-#[deriving(Clone)]
-pub struct UrlEncodedParser {
-    parse_url: bool,
-    parse_body: bool
-}
-
-impl UrlEncodedParser {
-    /// Creates a `UrlEncodedParser` which operates on both the URL and body
-    /// of a Request. This is less efficient than using `url_only` or `body_only`.
-    pub fn new() -> UrlEncodedParser {
-        UrlEncodedParser { parse_url: true, parse_body: true }
-    }
-
-    /// Creates a `UrlEncodedParser` which operates on the `url` field of a `Request`.
-    ///
-    /// Useful for parsing GET request query strings.
-    pub fn url_only() -> UrlEncodedParser {
-        UrlEncodedParser { parse_url: true, parse_body: false }
-    }
-
-    /// Creates a `UrlEncodedParser` which operates on the `body` field of a `Request`.
-    ///
-    /// Useful for parsing the bodies of POST requests.
-    pub fn body_only() -> UrlEncodedParser {
-        UrlEncodedParser { parse_url: false, parse_body: true }
-    }
-}
-
-impl Middleware for UrlEncodedParser {
-    fn enter(&mut self, req: &mut Request, _ : &mut Response) -> Status {
-        let mut result = UrlEncodedData { query_string: None, body: None };
-
-        // Parse the url's query string from the '?' characters onwards, if desired.
-        if self.parse_url {
-            result.query_string = req.url.query_pairs().map(combine_duplicates);
+impl UrlEncodedMixin for Request {
+    fn parse_urlencoded_query(&self) -> Option<HashMap<String, Vec<String>>> {
+        match self.url.query {
+            Some(ref query) => create_param_hashmap(query.as_slice()),
+            None => None
         }
+    }
 
-        // Parse the request's body if desired
-        if self.parse_body {
-            result.body = create_param_hashmap(req.body.as_slice());
-        }
-
-        // Insert the result into the Alloy
-        req.alloy.insert::<UrlEncodedData>(result);
-
-        Continue
+    fn parse_urlencoded_body(&self) -> Option<HashMap<String, Vec<String>>> {
+        create_param_hashmap(self.body.as_slice())
     }
 }
 
